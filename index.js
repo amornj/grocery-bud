48cfa3427e91c3b7c4717c6d2b47c5282b87c598
amornj <amornj@gmail.com>
2023-05-07 10:16:13 +0700

    add delete function and alert
    add delete and alert function

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ console.log(submitBtn);
 
 const list =document.querySelector('.grocery-list')
 console.log(list);
+
+const alert = document.querySelector('.alert');
 submitBtn.addEventListener('click', addItem);
 
 function addItem(event) {
@@ -15,20 +17,52 @@ function addItem(event) {
     const inputValue = groceryInput.value;
     console.log(inputValue);
     const newGroceryList = `
-    <div class="grocery-list">
-        <article class="grocery-item">
-            <p class="title">${inputValue}</p>
-            <div class="btn-container">
-                <button type="button" class="edit-btn">
-                    <i class="fa-regular fa-pen-to-square"></i>
-                </button>        
-                <button class="delete-btn">
-                    <i class="fa-regular fa-trash-can"></i>
-                </button>
+    // <div class="grocery-list">
+    //     <article class="grocery-item">
+    //         <p class="title">${inputValue}</p>
+    //         <div class="btn-container">
+    //             <button type="button" class="edit-btn">
+    //                 <i class="fa-regular fa-pen-to-square"></i>
+    //             </button>        
+    //             <button class="delete-btn">
+    //                 <i class="fa-regular fa-trash-can"></i>
+    //             </button>
 
-            </div>
-        </article>
-    </div>
-    `;
-    list.insertAdjacentHTML('afterend', newGroceryList);
+    //         </div>
+    //     </article>
+    // </div>
+    // `;
+    // list.insertAdjacentHTML('afterend', newGroceryList);
+    const element = document.createElement('article');
+    let attribute = document.createAttribute('data-id');
+    const id = new Date().getTime().toString();
+    attribute.value = id;
+    element.setAttributeNode(attribute)
+
+    element.classList.add("grocery-item");
+    element.innerHTML = ` <p class="title">${inputValue}</p>
+    <div class="btn-container">
+        <button type="button" class="edit-btn">
+            <i class="fa-regular fa-pen-to-square"></i>
+        </button>        
+        <button class="delete-btn">
+            <i class="fa-regular fa-trash-can"></i>
+        </button>
+    </div>`;
+    const deleteBtn = element.querySelector(".delete-btn");
+    deleteBtn.addEventListener('click', deleteItem);
+    list.appendChild(element);
+
+    alert.textContent= "item added to the list";
+    alert.classList.add('alert-success');
+    setTimeout(function(){
+        console.log('alert will disappear');
+        alert.textContent = "";
+        alert.classList.remove('alert-success');
+        }, 3000);          
+}
+
+function deleteItem(event) {
+    const element = event.currentTarget;
+    console.log(element);
 }
